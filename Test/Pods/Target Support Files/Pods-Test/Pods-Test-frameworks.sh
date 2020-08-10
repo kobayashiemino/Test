4a5e90eb435678bd0302c330401f18979d49c7fb
kobayashi emino <kobayashiemino0812@gmail.com>
2020-08-10 15:57:53 +0900

    GradientAnimatorを追加

--- Test/Pods/Target Support Files/Pods-Test/Pods-Test-frameworks.sh
+++ Test/Pods/Target Support Files/Pods-Test/Pods-Test-frameworks.sh
@@ -161,9 +161,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GradientAnimator/GradientAnimator.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TKSubmitTransition/TKSubmitTransition.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/GradientAnimator/GradientAnimator.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TKSubmitTransition/TKSubmitTransition.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
