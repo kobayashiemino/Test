a154640ce30446d2a934587b43cfeb6946585824
kobayashi emino <kobayashiemino0812@gmail.com>
2020-08-10 17:57:42 +0900

    messageLabel追加

--- Test/Test/QuestionSeetViewController.swift
+++ Test/Test/QuestionSeetViewController.swift
@@ -1,7 +1,11 @@
 import UIKit
 import GradientAnimator
+import MarqueeLabel
 
 class QuestionSeetViewController: UIViewController{
+    
+    private let viewWidth : CGFloat = UIScreen.main.bounds.width
+    private let viewHeight : CGFloat = UIScreen.main.bounds.height
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,5 +17,21 @@ class QuestionSeetViewController: UIViewController{
                                             _animationDuration: 3.0)
         self.view.insertSubview(gradientView, at: 0)
         gradientView.startAnimate()
+        
+        setupLabel(text: "")
+    }
+    
+    func setupLabel(text: String) {
+        let messageLabel = MarqueeLabel()
+        messageLabel.frame = CGRect(x: 20, y: 50, width: viewWidth - 40, height: 80)
+        messageLabel.center.x = viewWidth / 2
+        messageLabel.text = text
+        messageLabel.textColor = .white
+        messageLabel.font = UIFont(name: "HelveticaNeue-Light", size: 30)
+        messageLabel.type = .left
+        messageLabel.fadeLength = 20.0
+        messageLabel.animationDelay = 5.0
+        messageLabel.speed = .duration(30)
+        self.view.addSubview(messageLabel)
     }
 }
