f69a8a18bfdd258692bf433a9cb89b593097cdd6
kobayashi emino <kobayashiemino0812@gmail.com>
2020-08-10 17:56:47 +0900

    MarqueeLabel追加

--- Test/Pods/Target Support Files/Pods-Test/Pods-Test-frameworks.sh
+++ Test/Pods/Target Support Files/Pods-Test/Pods-Test-frameworks.sh
@@ -162,10 +162,12 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GradientAnimator/GradientAnimator.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MarqueeLabel/MarqueeLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TKSubmitTransition/TKSubmitTransition.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/GradientAnimator/GradientAnimator.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/MarqueeLabel/MarqueeLabel.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/TKSubmitTransition/TKSubmitTransition.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
