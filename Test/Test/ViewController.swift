883c16bcc491313b2957f66ab6ddfe790a53d70d
kobayashi emino <kobayashiemino0812@gmail.com>
2020-08-10 15:14:52 +0900

    TKSubmitTransitionを追加

--- Test/Test/ViewController.swift
+++ Test/Test/ViewController.swift
@@ -1,20 +1,12 @@
-//
-//  ViewController.swift
-//  Test
-//
-//  Created by kobayashi emino on 2020/08/10.
-//  Copyright © 2020 kobayashi emino. All rights reserved.
-//
-
 import UIKit
+import TKSubmitTransition
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.view.backgroundColor = .black
     }
-
-
 }
 
